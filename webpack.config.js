eb356ff5293a63c0a59141adce2225eeb8468e59
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-07-20 09:56:31 +0200

    Webpack Environment Setup

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    mode:'production',
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title:'ToDo | ProductivityApp',
+            template: './src/index.ejs',
+        }),
+    ],
+    output: {
+        filename: 'main.js',
+        path:path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+              },
+        ],
+    },
+}
