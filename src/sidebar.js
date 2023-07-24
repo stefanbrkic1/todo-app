fb30d3f6e826fd4878c9269c2745b7c3106073ac
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-07-24 16:10:51 +0200

    Added rename and delete buttons for project

--- src/sidebar.js
+++ src/sidebar.js
@@ -1,24 +1,33 @@
-export default function handleSidebar() {
-    const sidebarToggle = document.getElementById('Open');
-    const sidebar = document.getElementById('sidebarMenu')
-    const main = document.getElementById('mainSection');
-    const footer = document.getElementById('footerSection');
+const sidebarToggle = document.getElementById('Open');
+const sidebar = document.getElementById('sidebarMenu')
+const main = document.getElementById('mainSection');
+const footer = document.getElementById('footerSection');
 
+export default function handleSidebar() {
     sidebarToggle.addEventListener('change', function () {
         if (sidebarToggle.checked) {
             setTimeout(() => {
                 main.classList.add('blur');
                 footer.classList.add('blur');
             }, 155);
-            sidebar.classList.remove('smooth-transition')
+            handleTransition()
         }
         else {
             main.classList.remove('blur');
             footer.classList.remove('blur');
-            sidebar.classList.add('smooth-transition')
-            setTimeout(() => {
-                sidebar.classList.remove('smooth-transition')
-            }, 155);
+            handleTransition()
         }
     });
+}
+
+function handleTransition(){
+    if (sidebarToggle.checked) {
+        sidebar.classList.remove('smooth-transition')
+    }
+    else {
+        sidebar.classList.add('smooth-transition')
+        setTimeout(() => {
+            sidebar.classList.remove('smooth-transition')
+        }, 155);
+    }
 }
