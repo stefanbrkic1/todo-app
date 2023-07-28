b010d44e6210d6c7c526d79742e7fd9f50d39597
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-07-28 12:06:49 +0200

    Fixed adding projects, double invocation problem

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import handleSidebar from './sidebar'
 import loadAllTasks from './alltasks';
 import { ActiveTabHandler, ModalHandler } from './dom';
 import loadProjects from './projects';
+import { addProjectEvent } from './projects';
 
 (function websiteHandler(){
     const activeTabHandler = new ActiveTabHandler();
@@ -12,5 +13,6 @@ import loadProjects from './projects';
     loadAllTasks()
     activeTabHandler.handleTabsClick()
     loadProjects();
+    addProjectEvent()
     modalHandler.handleModals();
 })();
