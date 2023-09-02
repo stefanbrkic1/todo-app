1bc3430859659db0c6eb138a0a01fb7f1c7c46f6
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-09-02 15:31:15 +0200

    Added today tab functionality

--- src/today.js
+++ src/today.js
@@ -1,11 +1,30 @@
+import {format, isToday}  from 'date-fns'
 import todayIcon from './img/today48x48.svg'
+import noTasksIconThisWeek from './img/this-week-no-tasks.png'
+import { TasksHandler } from './dom'
+import { allProjects } from './projects'
 
+
+const tasksHandler = new TasksHandler()
 const tasksListView = document.getElementById('tasksListView')
 const headerSection = document.getElementById('headerSection')
 const floatingActionButton = document.getElementById('floatingActionButton')
 
 export default function loadTodayTasks() {
   setTodayTab()
+  allProjects.forEach(project => {
+    project.sections.forEach(section => {
+        section.tasks.forEach(task => {
+            if(taskIsToday(task.date)){
+                tasksHandler.createTaskHtml(tasksListView, task)
+            }
+            else{
+                return
+            }
+        })
+    })
+})
+displayNoTasksImage()
 }
 
 function setTodayTab(){
@@ -18,11 +37,46 @@ function setTodayTab(){
     currentTabIcon.innerHTML = ''
     currentTabIcon.appendChild(svgToday)
 
+    const todayDate = new Date()
+    const todayDateString =  format(todayDate, 'MMMM/dd/yyyy');
+
     currentTabName.textContent = 'Today'
+    tasksHandler.createRightDateDisplay(mainHeadingRight, todayDateString)
+
+    dateDisplayer.innerHTML = ''
+    tasksHandler.createDateDisplay(dateDisplayer, todayDateString)
 
     tasksListView.innerHTML = ''
-    dateDisplayer.innerHTML = ''
     floatingActionButton.innerHTML = ''
     headerSection.className = ''
     headerSection.classList.add('header-today')
+}
+
+function taskIsToday(taskDate){
+  let currentTaskDate = new Date(taskDate)
+  if(isToday(currentTaskDate) === true){
+      return true
+  }
+  else{
+      return false
+  }
+}
+
+function displayNoTasksImage(){
+  if(tasksListView.innerHTML === ''){
+      const container = document.createElement('div')
+      container.classList.add('no-tasks-img-container')
+
+      const noTasksImageThisWeek = new Image()
+      noTasksImageThisWeek.src = noTasksIconThisWeek
+      noTasksImageThisWeek.classList.add('no-tasks-img')
+
+      const text = document.createElement('div')
+      text.textContent = `You don't have any tasks for today`
+      text.classList.add('no-tasks-img-text')
+
+      container.appendChild(noTasksImageThisWeek)
+      container.appendChild(text)
+      tasksListView.appendChild(container)
+  }
 }
