d0ab650a7954a08ab21e7235130efdb6bb213111
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-09-10 22:23:13 +0200

    Added completing functioanlity to all tasks tab

--- src/alltasks.js
+++ src/alltasks.js
@@ -1,6 +1,8 @@
 import allTasksIcon from './img/alltasks48x48.svg'
 import { allProjects } from './projects'
 import { TasksHandler, ProjectsHandler } from './dom'
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 
 const tasksHandler = new TasksHandler()
 const projectsHandler = new ProjectsHandler()
@@ -10,32 +12,19 @@ const headerSection = document.getElementById('headerSection')
 const floatingActionButton = document.getElementById('floatingActionButton')
 const dateDisplayer = document.querySelector('.date-displayer')
 
+let allTasks = []
+
+class AllTask {
+    constructor(projectIndexAll, sectionIndexAll, taskIndexAll){
+        this.projectIndexAll = projectIndexAll
+        this.sectionIndexAll = sectionIndexAll
+        this.taskIndexAll = taskIndexAll
+    }
+}
+
 export default function loadAllTasks() {
     setAllTasksTab();
-    let tasksLength = 0
-    allProjects.forEach((project, projectIndex) => {
-        projectsHandler.createProjectHtmlForTabs(tasksListView, project);
-        const tasksContainerAllTasks = document.querySelectorAll('.tasks-container-all-tasks');
-
-        let hasTasks = false; // Track if there are tasks in the project
-
-        project.sections.forEach(section => {
-            if (section.tasks.length === 0) {
-
-            } else {
-                hasTasks = true; // Mark that there are tasks in this project
-                section.tasks.forEach(task => {
-                    tasksHandler.createTaskHtml(tasksContainerAllTasks[projectIndex], task);
-                    tasksLength++
-                });
-            }
-        });
-
-        if (!hasTasks) {
-            tasksHandler.displayNoTasksMessage(tasksContainerAllTasks[projectIndex]);
-        }
-    });
-    displayImportantTasksLength(tasksLength)
+    loadTasks()
 }
 
 function setAllTasksTab() {
@@ -57,16 +46,71 @@ function setAllTasksTab() {
     headerSection.classList.add('header-all-tasks')
 }
 
+function loadTasks(){
+    tasksListView.innerHTML = ''
+    allTasks = []
+    allProjects.forEach((project, projectIndexAll) => {
+        projectsHandler.createProjectHtmlForTabs(tasksListView, project);
+        const tasksContainerAllTasks = document.querySelectorAll('.tasks-container-all-tasks');
 
-function displayImportantTasksLength(tasksLength) {
+        let hasTasks = false; // Track if there are tasks in the project
+
+        project.sections.forEach((section, sectionIndexAll) => {
+            if (section.tasks.length === 0) {
+
+            } else {
+                hasTasks = true; // Mark that there are tasks in this project
+                section.tasks.forEach((task, taskIndexAll) => {
+                    tasksHandler.createTaskHtml(tasksContainerAllTasks[projectIndexAll], task);
+
+                    let newAllTask = new AllTask(projectIndexAll, sectionIndexAll, taskIndexAll)
+                    allTasks.push(newAllTask)
+                });
+            }
+        });
+
+        if (!hasTasks) {
+            tasksHandler.displayNoTasksMessage(tasksContainerAllTasks[projectIndexAll]);
+        }
+    });
+    displayTasksLength()
+    addAllTasksCheckboxEvent()
+}
+
+function displayTasksLength() {
     let tasksString = 'tasks'
-    const lengthArr = tasksLength.toString().split('')
-    if (lengthArr[lengthArr.length - 1] === '1') {
+    if (allTasks[allTasks.length - 1] === '1') {
         tasksString = 'task'
     }
-    const lengthString = `TasksCount: ${tasksLength} ${tasksString}.`
+    const lengthString = `TasksCount: ${allTasks.length} ${tasksString}.`
     tasksHandler.createRightDateDisplay(mainHeadingRight, lengthString)
 
     dateDisplayer.innerHTML = ''
     tasksHandler.createDateDisplay(dateDisplayer, lengthString)
+}
+
+function addAllTasksCheckboxEvent(){
+    let allCheckboxes = document.querySelectorAll('.checkbox')
+    allCheckboxes.forEach((checkbox, checkboxIndex) => {
+        checkbox.addEventListener('click', () => {
+            setTimeout(() => {
+                checkTaskAsCompleted(checkboxIndex, allTasks[checkboxIndex].projectIndexAll, allTasks[checkboxIndex].sectionIndexAll, allTasks[checkboxIndex].taskIndexAll)
+            }, 10);
+        })
+    })    
+}
+
+function checkTaskAsCompleted(checkboxIndex, projectIndexAll, sectionIndexAll, taskIndexAll){
+    allProjects[projectIndexAll].sections[sectionIndexAll].tasks.splice(taskIndexAll, 1)
+    allTasks.splice(checkboxIndex, 1)
+    setTimeout(() => {
+        loadTasks()
+        Toastify({
+            text: "1 task completed",
+            className: "custom-toast", // Apply the custom CSS class
+            duration: 3000,
+            gravity: "bottom", // `top` or `bottom`
+            position: "left", // `left`, `center` or `right`
+          }).showToast();
+    }, 300);
 }
