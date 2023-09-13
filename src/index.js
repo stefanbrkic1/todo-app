bf347243afa4795fd6d292adfb574e237cfdd865
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-09-13 21:33:44 +0200

    Added dark theme functionality

--- src/index.js
+++ src/index.js
@@ -5,26 +5,37 @@ import { ModalHandler } from './dom';
 import { loadProjects, addProjectEvent, addProjectToolsSubmitEvent } from './projects';
 
 (function websiteHandler(){
-    const modalHandler = new ModalHandler(); 
+    document.addEventListener("DOMContentLoaded", function() {
+        const modalHandler = new ModalHandler(); 
 
-    const preloaderForLoader = document.getElementById('preloaderForLoader')
-    const loader = document.getElementById('preloader')
-    window.addEventListener('load', function(){
-        preloaderForLoader.style.opacity = "0";
-        preloaderForLoader.style.visibility = "hidden";
-        setTimeout(() => {
-            loader.style.opacity = "0";
-            loader.style.visibility = "hidden";
-        }, 3500);
-    })
-
-    handleSidebar();
-    loadAllTasks();
-    loadProjects();
-    addProjectEvent();
-    addProjectToolsSubmitEvent()
-    modalHandler.handleModals();
-    modalHandler.changeModalPositionIfKeyboardOpened()
-    modalHandler.handleSubmit()
+        const preloaderForLoader = document.getElementById('preloaderForLoader')
+        const loader = document.getElementById('preloader')
+        window.addEventListener('load', function(){
+            preloaderForLoader.style.opacity = "0";
+            preloaderForLoader.style.visibility = "hidden";
+            setTimeout(() => {
+                loader.style.opacity = "0";
+                loader.style.visibility = "hidden";
+            }, 3500);
+        })
+        
+        handleSidebar();
+        loadAllTasks();
+        loadProjects();
+        addProjectEvent();
+        addProjectToolsSubmitEvent()
+        modalHandler.handleModals();
+        modalHandler.changeModalPositionIfKeyboardOpened()
+        modalHandler.handleSubmit()
+        setThemeTogglerEvent();
+      });
 })();
+
+function setThemeTogglerEvent(){
+    const themeToggler = document.getElementById('themeToggler')
+    themeToggler.addEventListener('click', () => {
+        console.log('clicked')
+        document.body.classList.toggle("dark-theme")
+    })
+}
 
