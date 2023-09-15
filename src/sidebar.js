ae73d659a42a7a30ba002e0daaecc9e19edad26e
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-09-15 23:09:11 +0200

    Added blur effect to logo on sidebar open

--- src/sidebar.js
+++ src/sidebar.js
@@ -3,6 +3,7 @@ const sidebar = document.getElementById('sidebarMenu')
 const main = document.getElementById('mainSection');
 const header = document.getElementById('headerSection');
 const floatingActionButton = document.getElementById('floatingActionButton');
+const listify = document.querySelector('.listify-container-small')
 
 export default function handleSidebar() {
     sidebarToggle.addEventListener('change', function () {
@@ -11,6 +12,7 @@ export default function handleSidebar() {
                 main.classList.add('blur');
                 header.classList.add('blur');
                 floatingActionButton.classList.add('blur');
+                listify.classList.add('blur');
                 sidebar.classList.add('active-sidebar')
             }, 220);
             handleTransition()
@@ -19,6 +21,7 @@ export default function handleSidebar() {
             main.classList.remove('blur');
             header.classList.remove('blur');
             floatingActionButton.classList.remove('blur');
+            listify.classList.remove('blur');
             sidebar.classList.remove('active-sidebar')
             handleTransition()
         }
