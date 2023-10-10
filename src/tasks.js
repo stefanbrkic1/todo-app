0bcd951ff5a8e6a2690bc686badc0975e1b5d5bd
stefanbrkic1 <stefanbrkicdzn.contact@gmail.com>
2023-10-10 15:39:05 +0200

    Notification update

--- src/tasks.js
+++ src/tasks.js
@@ -1,10 +1,10 @@
-import Toastify from 'toastify-js';
-import { TasksHandler, playNotificationSound } from './dom';
-import { currentProjectIndex } from './projects';
-import 'toastify-js/src/toastify.css';
+import Toastify from "toastify-js";
+import { TasksHandler, playNotificationSound } from "./dom";
+import { currentProjectIndex } from "./projects";
+import "toastify-js/src/toastify.css";
 
 const tasksHandler = new TasksHandler();
-const toastNotification = document.getElementById('toastNotification');
+const toastNotification = document.getElementById("toastNotification");
 
 class Task {
   constructor(description, date) {
@@ -17,17 +17,19 @@ class Task {
 let currentSectionIndex = null;
 
 export function addTaskFormCreationEvent() {
-  const addTaskButtons = document.querySelectorAll('.add-task-container');
+  const addTaskButtons = document.querySelectorAll(".add-task-container");
   addTaskButtons.forEach((button, index) => {
-    button.addEventListener('click', (e) => {
+    button.addEventListener("click", (e) => {
       const { target } = e;
       currentSectionIndex = index;
-      const currentSectionHtmlElement = target.closest('.section');
-      const currentSectionTasksContainer = currentSectionHtmlElement.querySelector('.form-task-container');
+      const currentSectionHtmlElement = target.closest(".section");
+      const currentSectionTasksContainer =
+        currentSectionHtmlElement.querySelector(".form-task-container");
       tasksHandler.createAddTaskFormHtml(currentSectionTasksContainer);
       addCancelButtonEvent(currentSectionTasksContainer);
-      const currentAddTaskButtonContainer = currentSectionHtmlElement.querySelector('.add-task-container');
-      currentAddTaskButtonContainer.classList.add('display-none');
+      const currentAddTaskButtonContainer =
+        currentSectionHtmlElement.querySelector(".add-task-container");
+      currentAddTaskButtonContainer.classList.add("display-none");
       removeAddTaskButtonsClick(addTaskButtons);
       addTaskSubmitEvent();
     });
@@ -35,9 +37,11 @@ export function addTaskFormCreationEvent() {
 }
 
 function addCancelButtonEvent(currentSectionTasksContainer) {
-  const cancelTaskFormButton = document.getElementById('cancelTaskFormButton');
-  cancelTaskFormButton.addEventListener('click', () => {
-    const addTaskForm = currentSectionTasksContainer.querySelector('.add-task-form-container');
+  const cancelTaskFormButton = document.getElementById("cancelTaskFormButton");
+  cancelTaskFormButton.addEventListener("click", () => {
+    const addTaskForm = currentSectionTasksContainer.querySelector(
+      ".add-task-form-container"
+    );
     if (addTaskForm) {
       addTaskForm.remove();
       addAddTaskButtons();
@@ -47,62 +51,64 @@ function addCancelButtonEvent(currentSectionTasksContainer) {
 
 function removeAddTaskButtonsClick(addTaskButtons) {
   addTaskButtons.forEach((button) => {
-    button.classList.add('disabled-click');
+    button.classList.add("disabled-click");
   });
-  const addTaskIcons = document.querySelectorAll('.add-task-btn');
+  const addTaskIcons = document.querySelectorAll(".add-task-btn");
   addTaskIcons.forEach((icon) => {
-    icon.classList.add('add-task-btn-disabled');
+    icon.classList.add("add-task-btn-disabled");
   });
 }
 
 function addAddTaskButtons() {
-  const addTaskButtons = document.querySelectorAll('.add-task-container');
+  const addTaskButtons = document.querySelectorAll(".add-task-container");
   addTaskButtons.forEach((button) => {
-    button.classList.remove('disabled-click');
-    button.classList.remove('display-none');
+    button.classList.remove("disabled-click");
+    button.classList.remove("display-none");
   });
 
-  const addTaskIcons = document.querySelectorAll('.add-task-btn');
+  const addTaskIcons = document.querySelectorAll(".add-task-btn");
   addTaskIcons.forEach((icon) => {
-    icon.classList.remove('add-task-btn-disabled');
+    icon.classList.remove("add-task-btn-disabled");
   });
 }
 
 function addTaskSubmitEvent() {
-  const addTaskFormButton = document.getElementById('addTaskFormButton');
-  const taskDescriptionInput = document.getElementById('taskDescriptionInput');
-  const dateInput = document.getElementById('dateInput');
-  const cancelTaskFormButton = document.getElementById('cancelTaskFormButton');
-  addTaskFormButton.addEventListener('click', () => {
-    if (taskDescriptionInput.value === '') {
-      taskDescriptionInput.classList.add('invalid-input');
-    } else if (dateInput.value === '') {
-      dateInput.classList.add('invalid-input');
+  const addTaskFormButton = document.getElementById("addTaskFormButton");
+  const taskDescriptionInput = document.getElementById("taskDescriptionInput");
+  const dateInput = document.getElementById("dateInput");
+  const cancelTaskFormButton = document.getElementById("cancelTaskFormButton");
+  addTaskFormButton.addEventListener("click", () => {
+    if (taskDescriptionInput.value === "") {
+      taskDescriptionInput.classList.add("invalid-input");
+    } else if (dateInput.value === "") {
+      dateInput.classList.add("invalid-input");
     } else {
       const newTask = new Task(taskDescriptionInput.value, dateInput.value);
-      const allProjectsLocal = JSON.parse(localStorage.getItem('allProjects'));
-      allProjectsLocal[currentProjectIndex].sections[currentSectionIndex].tasks.push(newTask);
-      localStorage.setItem('allProjects', JSON.stringify(allProjectsLocal));
+      const allProjectsLocal = JSON.parse(localStorage.getItem("allProjects"));
+      allProjectsLocal[currentProjectIndex].sections[
+        currentSectionIndex
+      ].tasks.push(newTask);
+      localStorage.setItem("allProjects", JSON.stringify(allProjectsLocal));
       loadAllSectionsTasks();
       cancelTaskFormButton.click();
     }
   });
 
-  taskDescriptionInput.addEventListener('focus', () => {
-    taskDescriptionInput.classList.remove('invalid-input');
-    dateInput.classList.remove('invalid-input');
+  taskDescriptionInput.addEventListener("focus", () => {
+    taskDescriptionInput.classList.remove("invalid-input");
+    dateInput.classList.remove("invalid-input");
   });
 
-  dateInput.addEventListener('focus', () => {
-    dateInput.classList.remove('invalid-input');
+  dateInput.addEventListener("focus", () => {
+    dateInput.classList.remove("invalid-input");
   });
 }
 
 export function loadAllSectionsTasks() {
-  const allProjectsLocal = JSON.parse(localStorage.getItem('allProjects'));
-  const allSectionTasksContainers = document.querySelectorAll('.section-tasks');
+  const allProjectsLocal = JSON.parse(localStorage.getItem("allProjects"));
+  const allSectionTasksContainers = document.querySelectorAll(".section-tasks");
   allSectionTasksContainers.forEach((container) => {
-    container.innerHTML = '';
+    container.innerHTML = "";
   });
   allProjectsLocal[currentProjectIndex].sections.forEach((section, index) => {
     section.tasks.forEach((task) => {
@@ -116,52 +122,60 @@ export function loadAllSectionsTasks() {
 let clickedSectionIndex = null;
 
 export function getCurrentSectionIndex() {
-  const allSectionsHtml = document.querySelectorAll('.section');
+  const allSectionsHtml = document.querySelectorAll(".section");
   allSectionsHtml.forEach((section, index) => {
-    section.addEventListener('click', () => {
+    section.addEventListener("click", () => {
       clickedSectionIndex = index;
     });
   });
 }
 
 export function addImportantButtonEvent() {
-  const allSectionsHtml = document.querySelectorAll('.section');
+  const allSectionsHtml = document.querySelectorAll(".section");
   allSectionsHtml.forEach((section) => {
-    const sectionImportantButtons = section.querySelectorAll('.important-btn');
+    const sectionImportantButtons = section.querySelectorAll(".important-btn");
     sectionImportantButtons.forEach((button, index) => {
-      button.addEventListener('click', () => {
-        setTimeout(() => {
-          if (!button.classList.contains('important-btn-active')) {
-            setTaskAsImportant(button, index);
-          } else if (button.classList.contains('important-btn-active')) {
-            setTaskAsUnimportant(button, index);
-          }
-        });
-      }, 500);
+      button.addEventListener(
+        "click",
+        () => {
+          setTimeout(() => {
+            if (!button.classList.contains("important-btn-active")) {
+              setTaskAsImportant(button, index);
+            } else if (button.classList.contains("important-btn-active")) {
+              setTaskAsUnimportant(button, index);
+            }
+          });
+        },
+        500
+      );
     });
   });
 }
 
 function setTaskAsImportant(clickedButton, index) {
-  const allProjectsLocal = JSON.parse(localStorage.getItem('allProjects'));
-  clickedButton.classList.add('important-btn-active');
-  allProjectsLocal[currentProjectIndex].sections[clickedSectionIndex].tasks[index].important = true;
-  localStorage.setItem('allProjects', JSON.stringify(allProjectsLocal));
+  const allProjectsLocal = JSON.parse(localStorage.getItem("allProjects"));
+  clickedButton.classList.add("important-btn-active");
+  allProjectsLocal[currentProjectIndex].sections[clickedSectionIndex].tasks[
+    index
+  ].important = true;
+  localStorage.setItem("allProjects", JSON.stringify(allProjectsLocal));
 }
 
 function setTaskAsUnimportant(clickedButton, index) {
-  const allProjectsLocal = JSON.parse(localStorage.getItem('allProjects'));
-  clickedButton.classList.remove('important-btn-active');
-  allProjectsLocal[currentProjectIndex].sections[clickedSectionIndex].tasks[index].important = false;
-  localStorage.setItem('allProjects', JSON.stringify(allProjectsLocal));
+  const allProjectsLocal = JSON.parse(localStorage.getItem("allProjects"));
+  clickedButton.classList.remove("important-btn-active");
+  allProjectsLocal[currentProjectIndex].sections[clickedSectionIndex].tasks[
+    index
+  ].important = false;
+  localStorage.setItem("allProjects", JSON.stringify(allProjectsLocal));
 }
 
 function addTaskCheckboxEvent() {
-  const allSectionsHtml = document.querySelectorAll('.section');
+  const allSectionsHtml = document.querySelectorAll(".section");
   allSectionsHtml.forEach((section) => {
-    const sectionCheckboxes = section.querySelectorAll('.checkbox');
+    const sectionCheckboxes = section.querySelectorAll(".checkbox");
     sectionCheckboxes.forEach((checkbox, checkboxIndex) => {
-      checkbox.addEventListener('click', () => {
+      checkbox.addEventListener("click", () => {
         setTimeout(() => {
           checkTaskAsCompleted(checkboxIndex);
           playNotificationSound();
@@ -172,17 +186,19 @@ function addTaskCheckboxEvent() {
 }
 
 function checkTaskAsCompleted(checkboxIndex) {
-  const allProjectsLocal = JSON.parse(localStorage.getItem('allProjects'));
-  allProjectsLocal[currentProjectIndex].sections[clickedSectionIndex].tasks.splice(checkboxIndex, 1);
-  localStorage.setItem('allProjects', JSON.stringify(allProjectsLocal));
+  const allProjectsLocal = JSON.parse(localStorage.getItem("allProjects"));
+  allProjectsLocal[currentProjectIndex].sections[
+    clickedSectionIndex
+  ].tasks.splice(checkboxIndex, 1);
+  localStorage.setItem("allProjects", JSON.stringify(allProjectsLocal));
   setTimeout(() => {
     loadAllSectionsTasks();
     Toastify({
-      text: '1 task completed',
-      className: 'custom-toast', // Apply the custom CSS class
+      text: "1 task completed",
+      className: "custom-toast", // Apply the custom CSS class
       duration: 2300,
-      gravity: 'bottom', // `top` or `bottom`
-      position: 'left', // `left`, `center` or `right`
+      gravity: "bottom", // `top` or `bottom`
+      position: "left", // `left`, `center` or `right`
     }).showToast();
   }, 300);
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,63 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    mode:'development',
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: path.resolve(__dirname, 'dist'), // Set the path to your output folder (Webpack 5+)
-        hot: true, // Enable hot module replacement for faster updates
-        liveReload: false,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title:'ToDo | ProductivityApp',
-            favicon: "./src/img/task-green.png",
-            template: path.resolve(__dirname, './src/template.html'),
-        }),
-    ],
-    output: {
-        filename: '[name].[contenthash].js',
-        path:path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: path.resolve(__dirname, "dist"), // Set the path to your output folder (Webpack 5+)
+    hot: true, // Enable hot module replacement for faster updates
+    liveReload: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "ToDo | ProductivityApp",
+      favicon: "./src/img/task-green.png",
+      template: path.resolve(__dirname, "./src/template.html"),
+    }),
+  ],
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(mp3|wav)$/, // Add any other audio file formats you need to support
+        use: [
+          {
+            loader: "file-loader", // or 'url-loader' if you prefer
+            options: {
+              name: "[name].[ext]",
+              outputPath: "audio/", // Specifies where the bundled audio files will be placed
             },
-            {
-                test: /\.(mp3|wav)$/, // Add any other audio file formats you need to support
-                use: [
-                  {
-                    loader: 'file-loader', // or 'url-loader' if you prefer
-                    options: {
-                      name: '[name].[ext]',
-                      outputPath: 'audio/', // Specifies where the bundled audio files will be placed
-                    },
-                  },
-                ],
-              },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-              },
+          },
         ],
-    },
-}
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+          },
+        },
+      },
+    ],
+  },
+};
